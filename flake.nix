fc7945ed11e561dc6b63b82667459166f360572d
o-tsaruk <oleksandr.v.tsaruk@gmail.com>
2023-07-13 23:25:10 +0300

    flake update

--- flake.nix
+++ flake.nix
@@ -1,9 +1,12 @@
 {
   inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+  inputs.nix-filter.url = "github:numtide/nix-filter";
+  inputs.flake-utils.url = "github:numtide/flake-utils";
 
   outputs = {
     self,
     nixpkgs,
+    nix-filter,
     flake-utils,
   }:
     flake-utils.lib.eachDefaultSystem
@@ -11,14 +14,48 @@
       pkgs = import nixpkgs {
         inherit system;
       };
-    in {
-      packages.default = pkgs.mkShell {
-        src = ./.;
-        buildInputs = [
-          pkgs.nodejs-18_x
-          pkgs.nodePackages.typescript
-          pkgs.nodePackages.typescript-language-server
+
+      src = nix-filter.lib.filter {
+        root = ./.;
+
+        include = [
+          "public"
+          "src"
+          ./package.json
+          ./package-lock.json
+          ./tsconfig.json
         ];
+      };
+
+      mkFrontend = REACT_APP_SERVER_URL:
+        pkgs.buildNpmPackage {
+          inherit REACT_APP_SERVER_URL src;
+
+          pname = "patron-ui";
+          version = "0.1.0";
+
+          npmDepsHash = "sha256-76hd+8m6FHK3jAeXXJqihG6NnAI+03vyoPf6g+iul6g=";
+
+          installPhase = ''
+            cp -r build $out
+          '';
+        };
+    in {
+      formatter = pkgs.alejandra;
+      packages = {
+        development = mkFrontend "http://localhost:3000";
+        production = mkFrontend "https://api.patron.works";
+      };
+      apps.default = flake-utils.lib.mkApp {
+        drv = let
+          root = self.packages.${system}.development;
+        in pkgs.writeShellScriptBin "dev-server" ''
+          ${pkgs.lib.getExe pkgs.static-web-server} \
+            --port 8080 \
+            --root ${root} \
+            --page-fallback ${root}/index.html \
+            --log-level info
+        '';
       };
     });
 }
